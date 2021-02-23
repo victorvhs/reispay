750fe55b3df50ed75a2de9494eb8d3f6dd60014c
Victor Reis <victor.h.s.reis@gmail.com>
2021-02-23 09:35:12 -0300

    sum from file

--- lib/reispay/numbers.ex
+++ lib/reispay/numbers.ex
@@ -1,5 +1,19 @@
 defmodule Reispay.Numbers do
   def sum_from_file(filename) do
-   {:ok, file} = File.read("#{filename}.cvs")
+   "#{filename}.csv"
+   |> File.read()
+   |> handle_file()
   end
+
+  defp handle_file({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(fn num -> String.to_integer(num) end)
+      |> Enum.sum()
+
+    {:ok, %{result: result}}
+  end
+
+  defp handle_file({:error, _reason}), do: {:error, "Arquivo invalido"}
 end
